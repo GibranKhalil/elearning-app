0e91c322588bc7560c580d1b9b6f8128b0e2745a
Gibran Khalil <gibrankhalil150904@gmail.com>
2024-08-30 12:28:20 -0300

    Criação do caminho das lições

--- elearning/src/app/ui/pages/course/course.component.ts
+++ elearning/src/app/ui/pages/course/course.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { SectionHeaderComponent } from "../../components/section-header/section-header.component";
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [HeaderComponent, SectionHeaderComponent],
+  imports: [HeaderComponent, SectionHeaderComponent, CommonModule],
   templateUrl: './course.component.html',
-  styleUrl: './course.component.scss'
+  styleUrls: ['./course.component.scss']
 })
 export class CoursePage {
+
+  circles = Array.from({ length: 15 }); // Cria um array com 13 elementos, ajustável
+
+  private leftValues = [0, -40, -70, -40, 0, 40, 70, 40];
+  private topValues = [24, 12, 12, 20];
+
+  getCircleStyle(index: number): { [key: string]: string } {
+    const leftIndex = index % this.leftValues.length;
+    const topIndex = index % this.topValues.length;
+
+    return {
+      'left': `${this.leftValues[leftIndex]}px`,
+      'margin-top': `${this.topValues[topIndex]}px`
+    };
+  }
 
 }
